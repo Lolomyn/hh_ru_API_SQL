02c530cc5cf719a99115e1723592e295853f245e
Lolomyn <61377364+Lolomyn@users.noreply.github.com>
2025-04-07 23:37:57 +0300

    Revert "Feature/course work"

--- main.py
+++ main.py
@@ -1,51 +1,16 @@
-from src.config import config
-from src.db_manager import DBManagerPostgres, HeadHunterAPI
+# This is a sample Python script.
 
-hh_api = HeadHunterAPI()
-params = config()
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-if __name__ == "__main__":
 
-    employer_ids = [
-        78638,  # Т-БАНК
-        9498112,  # Яндекс.Крауд
-        3529,  # СБЕР
-        6111353,  # SWOYO
-        2180,  # OZON
-        3776,  # МТС
-        15478,  # VK
-        1993194,  # YADRO
-        6093775,  # ASTON
-        80,  # ALFA-BANK
-        3707941,  # Цитадель
-    ]
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-    employers = hh_api.get_employers(employer_ids)
 
-    vacancies = []
-    for employer in employers:
-        vacancies.extend(hh_api.get_vacancies(employer["id"]))
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
 
-    db_name = input("Введите имя для базы данных: ")
-    db_manager = DBManagerPostgres(params)
-
-    db_manager.create_database(db_name, params)
-    db_manager.create_tables()
-    db_manager.save_employers_to_db(employers)
-    db_manager.save_vacancies_to_db(vacancies)
-
-    # Примеры запросов
-    print("Компании и количество вакансий:")
-    print(db_manager.get_companies_and_vacancies_count())
-
-    print("\nВсе вакансии:")
-    print(db_manager.get_all_vacancies())
-
-    print("\nСредняя зарплата по вакансиям:")
-    print(db_manager.get_avg_salary())
-
-    print("\nВакансии, где зарплата выше средней:")
-    print(db_manager.get_vacancies_with_higher_salary())
-
-    print("\nВакансии по ключевому слову 'Python':")
-    print(db_manager.get_vacancies_with_keyword("Python"))
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
